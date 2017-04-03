26bddf0f89ac5d6c0038026e02bcb6bfa55e37ef
Timur Kiyivinski <deathseclipse@gmail.com>
2017-04-04 00:55:16 +0800

    [REFACTOR] TP [ADD] TP Coordinates

--- src/index.js
+++ src/index.js
@@ -17,16 +17,28 @@ function main (sources) {
     .map(chat => Object.assign({}, chat, { command: chat.message.split(' ') }))
 
   const tp$ = command$
-    .filter(chat => chat.command.length === 3)
+    .filter(chat => chat.command.length === 3 || chat.command.length === 5)
     .filter(chat => chat.command[1] === 'tp')
+
+  const tpUser$ = tp$
+    .filter(chat => chat.command.length === 3)
     .map(chat => ({
       action: 'chat',
       args: [`/tp ${chat.username} ${chat.command[2]}`]
     }))
 
+  const tpCoordinate$ = tp$
+    .filter(chat => chat.command.length === 5)
+    .map(chat => ({
+      action: 'chat',
+      args: [`/execute ${chat.username} ~ ~ ~ tp @p ${chat.command[2]} ${chat.command[3]} ${chat.command[4]}`]
+    }))
+
+  const minecraft$ = xs.merge(tpUser$, tpCoordinate$)
+
   return {
     log: chat$,
-    minecraft: tp$
+    minecraft: minecraft$
   }
 }
 
